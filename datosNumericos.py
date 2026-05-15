3e691fecf66604b9471fc7854277853bf1d0257d
Student <student@example.com>
2026-05-15 09:14:09 +0000

    pythom lab1

---
+++ datosNumericos.py
@@ -0,0 +1,30 @@
+print("Python tiene tres tipos numéricos: int, float y complex")
+print("1. int")
+miValor=1
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de datos " + str(type(miValor)))
+
+print("2. float")
+miValor=3.14
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de datos " + str(type(miValor)))
+
+print("3. complex")
+miValor=5j
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de datos " + str(type(miValor)))
+
+print("4.1. boolean")
+myValue=True
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de datos " + str(type(miValor)))
+
+print("4.2. boolean")
+myValue=False
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de datos " + str(type(miValor)))
